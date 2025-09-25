c8c83fba5c113f69c2f3471e7cb2cd2a12f7d4b9
Mukhammad Rizki <rizkyRE7@gmail.com>
2025-09-25 11:06:46 +0700

    MallHub Initialized

---
+++ app/Models/ProductImage.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductImage extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'product_id',
+        'url'
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
---
+++ app/Models/SubCategory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SubCategory extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'category_id',
+        'name'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
